9ea82ed9b57c059d6346e0efea22ec35ed6890ae
joergrieger <joergrieger@users.noreply.github.com>
2017-09-18 10:26:30 +0200

    Delete facrot.r

--- R/facrot.r
+++
@@ -1,9 +0,0 @@
-facrot <- function(F,Ffast,Fslow){
-  
-  Ffast <- as.matrix(Ffast)
-  k1    <- ncol(Ffast)
-  fm    <- cbind(matrix(1,nrow(Ffast),1), Ffast, Fslow)
-  b     <- olssvd(F,fm)
-  Fr <- F-Ffast%*%b[2:(k1+1),]
-  return(Fr)
-}
