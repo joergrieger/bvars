475088a4aa86694ceabe6fbfe34a46cbeca3bde3
joergrieger <joergrieger@users.noreply.github.com>
2019-07-30 22:13:58 +0200

    Added forecasts for threshold vars

--- R/tvar.r
+++ R/tvar.r
@@ -183,7 +183,8 @@ tvar <- function(mydata,priorObj,thMax=2,thVar=1,nreps = 1100,burnin=100,nthin=1
                               nreps     = nreps,
                               burnin    = burnin,
                               nthin     = nthin,
-                              thVar     = thVar)
+                              thVar     = thVar,
+                              thMax     = thMax)
 
 
   if(sum(class(mydata) == "xts") > 0){
@@ -337,5 +338,126 @@ irf.tvar  <- function(obj,id_obj, nhor = 12, ncores = 1, irfquantiles = c(0.05,0
   relist <- structure(list(irf=irffinal,irfhorizon=nhor,varnames=obj$general_info$var_names),class = "tvirf")
 
   return(relist)
+
+}
+#' @export
+#' @title forecast threshold vector autoregression
+#' @param obj S3 object from tvar
+#' @param forecastHorizon Forecast Horizon
+#' @param interval forecast interval
+#' @param ... currently not used
+forecast.tvar <- function(obj,forecastHorizon,interval= c(0.05,0.95),...){
+
+
+  # Preliminary Calculations
+  nVariables       <- dim(obj$data_info$data)[2] # Number of variables
+  nLength          <- dim(obj$data_info$data)[1] # Length of time series
+  nLags            <- obj$general_info$nolags  # Number of lags
+  nForecasts       <- dim(obj$mcmc_draws$Alpha)[4] # Number of forecasts, depends on the number sampled posteriors
+
+  nl1 <- max(obj$general_info$thMax,obj$gneral_info$nolags)
+  mForecastStorage <- array(NA,dim=c(forecastHorizon+nl1,nVariables,nForecasts)) # Matrix to storage forecasts
+
+  print(dim(mForecastStorage))
+
+
+  if(is.ts(obj$data_info$data)){
+
+    tsStart          <- start(obj$data_info$data)
+    tsFrequency      <- frequency(obj$data_info$data)
+
+  }
+
+  for(ii in 1:nForecasts){
+
+    mForecastStorage[1:nl1,,ii] <- (obj$data_info$data[nLength:(nLength-nl1+1),])
+
+    for(jj in 1:forecastHorizon){
+
+      nStart <- jj
+      nEnd   <- jj+nLags-1
+      y <- mForecastStorage[nStart:nEnd,,ii]
+      thCheck <- mForecastStorage[jj + nl1 - obj$mcmc_draws$deldraws[ii] ,obj$general_info$thVar, ii]
+
+      if(thCheck < obj$mcmc_draws$tardraws[ii]){
+
+        # First Regime
+        Alpha <- obj$mcmc_draws$Alpha[,,1,ii]
+        Sigma <- obj$mcmc_draws$Sigma[,,1,ii]
+
+      }
+      else{
+
+        # Second Regime
+        Alpha <- obj$mcmc_draws$Alpha[,,2,ii]
+        Sigma <- obj$mcmc_draws$Sigma[,,2,ii]
+
+      }
+
+      randDraw <- rnorm(nVariables) %*% t(chol(Sigma))
+
+      if(obj$general_info$intercept){
+
+        y <- c(1,t(y))
+        tempForecast <- y %*% Alpha + randDraw
+
+      }
+      else{
+        y <- t(y)
+        tempForecast <- y %*% Alpha + randDraw
+      }
+
+      # Storing the forecast
+      mForecastStorage[jj+nl1,,ii] <- tempForecast
+
+    }
+
+  }
+  # Remove initial values
+  mForecast <- mForecastStorage[-c(1:nl1),,]
+  forecastMean  <- array(0,dim=c(forecastHorizon,nVariables))
+  forecastUpper <- array(0,dim=c(forecastHorizon,nVariables))
+  forecastLower <- array(0,dim=c(forecastHorizon,nVariables))
+  for(ii in 1:nVariables){
+    for(jj in 1:forecastHorizon){
+
+      forecastMean[jj,ii]  <- mean(mForecast[jj,ii,])
+      forecastUpper[jj,ii] <- quantile(mForecast[jj,ii,],probs=max(interval))
+      forecastLower[jj,ii] <- quantile(mForecast[jj,ii,],probs=min(interval))
+
+    }
+  }
+
+  #forecastFinalMean  <- rbind(as.matrix(tvarObj$mydata),forecastMean)
+  #forecastFinalUpper <- rbind(as.matrix(tvarObj$mydata),forecastUpper)
+  #forecastFinalLower <- rbind(as.matrix(tvarObj$mydata),forecastLower)
+  OriginalPath       <- array(NA,dim=c(nLength + forecastHorizon, nVariables))
+  forecastFinalMean  <- array(NA,dim=c(nLength + forecastHorizon, nVariables))
+  forecastFinalUpper <- array(NA,dim=c(nLength + forecastHorizon, nVariables))
+  forecastFinalLower <- array(NA,dim=c(nLength + forecastHorizon, nVariables))
+
+  OriginalPath[1:nLength,] <-obj$data_info$data
+  forecastFinalMean[(nLength + 1):(nLength + forecastHorizon),] <- forecastMean
+  forecastFinalUpper[(nLength + 1):(nLength + forecastHorizon),] <- forecastUpper
+  forecastFinalLower[(nLength + 1):(nLength + forecastHorizon),] <- forecastLower
+
+  if(is.ts(obj$data_info$data)){
+
+    forecastFinalMean  <- ts(forecastFinalMean,start=tsStart,frequency=tsFrequency)
+    forecastFinalUpper <- ts(forecastFinalUpper,start=tsStart,frequency=tsFrequency)
+    forecastFinalLower <- ts(forecastFinalLower,start=tsStart,frequency=tsFrequency)
+    OriginalPath       <- ts(OriginalPath,start=tsStart,frequency=tsFrequency)
+
+  }
+
+  colnames(forecastFinalMean)  <- colnames(obj$data_info$data)
+  colnames(forecastFinalUpper) <- colnames(obj$data_info$data)
+  colnames(forecastFinalLower) <- colnames(obj$data_info$data)
+  colnames(OriginalPath)       <- colnames(obj$data_info$data)
+
+  retList <- structure(list(forecast = forecastFinalMean, Upper = forecastFinalUpper, Lower = forecastFinalLower,
+                            Original = OriginalPath),class="fcbvar")
+  return(retList)
+
 
 }
--- tests/test-models.r
+++ tests/test-models.r
@@ -24,9 +24,9 @@ msestimate <- msvar(mydata=USMonPol,noregimes=2, priorObj = prior,stabletest = T
 # Estimate Threshold VAR with ssvs-prior
 #
 
-prior <- set_prior_ssvs(mydata=USMonPol,nolags=2,tau=10,kappa=10)
-prior$kappa0 = 1
-prior$tau0   = 1
+prior <- set_prior_ssvs(mydata=USMonPol,nolags=3,tau=100,kappa=100)
+prior$kappa0 = 0.1
+prior$tau0   = 0.1
 tvestimate <- tvar(mydata = USMonPol,priorObj = prior,thMax = 10, thVar = 2,nthin = 5, nreps = 110,burnin = 10)
 
 # Get Impulse-Response-Functions
@@ -52,3 +52,5 @@ plot(msirfestimate)
 fc <- forecast(bvestimate,forecastHorizon = 6)
 plot(fc)
 
+fctv <- forecast(tvestimate,forecastHorizon = 6)
+plot(fctv)
